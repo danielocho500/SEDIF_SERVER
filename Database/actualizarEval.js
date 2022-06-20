e4425891c4c73ddb73c133be25bbe20ed54d0cfb
danielocho500 <danielnochess@gmail.com>
2022-06-20 09:43:24 -0500

    e

--- Database/actualizarEval.js
+++ Database/actualizarEval.js
@@ -1,11 +1,11 @@
 const sql = require('mssql');
 const { sqlConfig } = require('../models/databaseConfig');
 
-const actualizarEval = async (idTramite, status, observaciones ) => {
+const actualizarEval = async (idTramite,status) => {
     try{
         let pool = await sql.connect(sqlConfig)
         let eval = await pool.request().query(`UPDATE Evaluaciones SET
-                                                status = ${status}, observaciones = ${observaciones}
+                                                status = ${status}
                                                 WHERE idTramite = ${idTramite}`)
 
         return true
